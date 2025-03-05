3f55fd6175341511aad32abc1e6bb2091c07a3d1
damileeds <damileeds@gmail.com>
2025-03-05 18:49:32 +0000

    Asteroids are now SPAWNING!

--- player.py
+++ player.py
@@ -36,7 +36,7 @@ class Player(CircleShape):
             self.rotate(dt)
         if keys[pygame.K_w]:
             self.move(dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_s]:
             self.move(-dt)
 
     
